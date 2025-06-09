4ad73df0cc49e98ef0b996f100acfb611c924824
its_saravanan <saravanan.s@inflexiontechfze.com>
2025-06-09 19:51:50 +0530

    Tailwind problem

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import Body from "./components/Body";
+
+const App = () => {
+    return <>
+        <Header />
+        <Body />
+    </>;
+}
+
+export default App;
---
+++ src/components/Body.jsx
@@ -0,0 +1,12 @@
+const Body = () => {
+    return <>
+        <div className="grid grid-cols-4 gap-4">
+            <button className="border-4 border-white p-20 m-10 flex justify-center cursor-pointer bg-blue-400 rounded-3xl bg-violet-500 hover:bg-violet-600 focus:outline-4 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700">Cards</button>
+            <button className="border-4 border-white p-20 m-10 flex justify-center cursor-pointer bg-blue-400 rounded-3xl bg-violet-500 hover:bg-violet-600 focus:outline-4 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700">Cards</button>
+            <button className="border-4 border-white p-20 m-10 flex justify-center cursor-pointer bg-blue-400 rounded-3xl bg-violet-500 hover:bg-violet-600 focus:outline-4 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700">Cards</button>
+            <button className="border-4 border-white p-20 m-10 flex justify-center cursor-pointer bg-blue-400 rounded-3xl bg-violet-500 hover:bg-violet-600 focus:outline-4 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700">Cards</button>
+        </div>
+    </>;
+}/* border-4  p-20 m-10 flex justify-center cursor-pointer bg-blue-400 rounded-3xl bg-violet-500 */
+
+export default Body;
